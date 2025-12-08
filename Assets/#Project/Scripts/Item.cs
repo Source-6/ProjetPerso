dd2b1983a323389d522bf8ba834c09dc00c2e889
source <vandermeirschmanon@gmail.com>
2025-12-08 16:31:44 +0100

    afternoon pause

--- Assets/#Project/Scripts/Item.cs
+++ Assets/#Project/Scripts/Item.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 
 public enum ItemType
@@ -6,8 +7,17 @@ public enum ItemType
     Glue,
     Honey,
     Trap,
-    }
+}
+
+[RequireComponent(typeof(NavMeshObstacle))]
 public class Item : MonoBehaviour
 {
     public ItemType itemType;
+    private NavMeshObstacle navMeshObstacle;
+    void Start()
+    {
+        navMeshObstacle = GetComponent<NavMeshObstacle>();
+        navMeshObstacle.carving = true;
+    }
+
 }
